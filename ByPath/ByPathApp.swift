45a9f3bcab0c656b5d831d83f57c495720b40909
Anahita Zahertar <ana.zahertar@gmail.com>
2022-11-18 10:18:54 -0500

    Initial Commit

---
+++ ByPath/ByPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByPathApp.swift
+//  ByPath
+//
+//  Created by Anahita Zahertar on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ByPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
